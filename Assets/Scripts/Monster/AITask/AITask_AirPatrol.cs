85b08d316ee1268897ba45852c8f2a740c1a910e
김태현 <th2252@gmail.com>
2024-01-16 22:23:45 +0900

    [Fix] 공중 적이 지면과 닿았을 때 즉각 다음 행동을 하던 현상 수정
    공중 적이 이제 '앞' 방향에 대해서만 Terrain check를 실시함.
    지형에 가로막혔다고 판단했을 때, 즉각 다음 목적지를 설정 및 이동하지 않고 우선 현재 행동을 Succeed(성공으로 판정)하도록 설정

--- Assets/Scripts/Monster/AITask/AITask_AirPatrol.cs
+++ Assets/Scripts/Monster/AITask/AITask_AirPatrol.cs
@@ -8,44 +8,54 @@ using System;
 
 public class AITask_AirPatrol : MonoBehaviour
 {
-    // 컴포넌트 레퍼런스
+    [Title("컴포넌트 레퍼런스")]// 컴포넌트 레퍼런스
     [SerializeField]
     protected Blackboard blackboard;
     [SerializeField]
     private new Rigidbody2D rigidbody;
 
-    // 순찰 범위 관련
-    [SerializeField, Tooltip("순찰 범위를 시작점 근처로 제한할 것인지 여부")]
-    private bool moveOnlyNearby;
-
-    // 순찰 목적지 선정 관련
-    [SerializeField, Tooltip("다음 목적지 선호도 시각화")]
-    private bool drawDestDecisionVector;
-    [SerializeField, Tooltip("다음 목적지 선정 시 선호도에 랜덤 요소 반영")]
+    [Title("순찰 목적지 선정 옵션")]// 순찰 목적지 선정 관련
+    [SerializeField, Tooltip("다음 방향 선정 시 최고 선호도대신 선호도 가중치를 사용하는 랜덤 사용")]
     private bool useRandomToPreference;
     [ShowIf("useRandomToPreference")]
-    [SerializeField, Tooltip("랜덤 요소 사용 시 가중치 지수")]
-    private float randomPow;
+    [SerializeField, Range(0.3f, 3f), Tooltip("랜덤 요소 사용 시 가중치 지수")]
+    private float randomWeightExp;
     [SerializeField, Tooltip("다음 목적지까지의 최소 거리")]
     private float minPatrolDist = 1f;
     [SerializeField, Tooltip("다음 목적지까지의 최대 거리")]
     private float maxPatrolDist = 4f;
 
-    [Space(10)]
+    [FoldoutGroup("AI 계수")]
+    [SerializeField, Tooltip("순찰 범위의 중심으로 가려는 성질")]
+    private float aiCoef_toCenter = 1;
+    [FoldoutGroup("AI 계수")]
+    [SerializeField, Tooltip("벽에서 먼 곳으로 향하려는 성질")]
+    private float aiCoef_awayFromWall = 0.3f;
+    [FoldoutGroup("AI 계수")]
+    [SerializeField, Tooltip("순찰 범위의 중심을 중점으로 하는 원형 궤도로 움직이려는 성질")]
+    private float aiCoef_sidestep = 0.6f;
+
+    [Title("타이밍과 속도")]
     [SerializeField, Tooltip("순찰 사이사이 멈추는 시간 길이")]
-    private float patrolWaitTime;
+    private float patrolStopTime;
     [SerializeField, Tooltip("순찰 시의 이동 속도")]
     private float patrolSpeed;
 
-    [BoxGroup("AiCoefficient")]
-    [SerializeField]
-    private float aiCoef_toCenter = 1;
-    [BoxGroup("AiCoefficient")]
-    [SerializeField]
-    private float aiCoef_awayFromWall = 0.3f;
-    [BoxGroup("AiCoefficient")]
-    [SerializeField]
-    private float aiCoef_sidestep = 0.6f;
+    [Title("지형 감지 관련")]
+    [SerializeField, Tooltip("지형 감지 콜라이더가 달려있는 오브젝트")]
+    private GameObject terrainSensor;
+    [SerializeField, Tooltip("지형 감지 콜라이더를 몬스터 중심으로부터 얼만큼 떨어뜨릴건지")]
+    private float terrainSensorOffset;
+
+    [FoldoutGroup("시각화 옵션")]
+    [SerializeField, Tooltip("순찰 중심점 표시")]
+    private bool drawPatrolCenter;
+    [FoldoutGroup("시각화 옵션")]
+    [SerializeField, Tooltip("다음 방향 선호도 표시")]
+    private bool drawDestDecision;
+    [FoldoutGroup("시각화 옵션")]
+    [SerializeField, Tooltip("현재 순찰 포인트 표시")]
+    private bool drawDestPoint;
 
     // 순찰 범위(원형)의 중심. Start 시점에 이 몬스터가 위치한 곳을 중심으로 삼음.
     private Vector2 patrolAreaCenter;
@@ -64,7 +74,6 @@ public class AITask_AirPatrol : MonoBehaviour
                                     (Vector2.left + 2 * Vector2.up).normalized };   // 북북서
     // 16방향으로 움직일 때, 어느 방향으로 갈지 계산한 선호도.
     private float[] dirPreference = new float[16];
-    [SerializeField]
     private Vector2 nextDest;
     private Timer waitTimer = null;
     private WeightedRandomPicker<int> randomPicker;
@@ -78,7 +87,13 @@ public class AITask_AirPatrol : MonoBehaviour
     [Task]
     private void Patrol()
     {
-        if(Vector2.Distance((Vector2)transform.position, nextDest) < 0.1f)
+        bool isStuckAtWall;
+        blackboard.TryGet(BBK.StuckAtWall, out isStuckAtWall);
+        if (isStuckAtWall)
+        {
+            ThisTask.Succeed();
+        }
+        else if (Vector2.Distance((Vector2)transform.position, nextDest) < 0.1f)
         {
             ThisTask.Succeed();
         }
@@ -90,21 +105,6 @@ public class AITask_AirPatrol : MonoBehaviour
 
     private void MoveTo(Vector2 dest)
     {
-        bool isStuckAtWall;
-        blackboard.TryGet(BBK.StuckAtWall, out isStuckAtWall);
-        if (isStuckAtWall)
-        {
-            // 수동으로 목적지 재설정
-            int index = 0;
-            for (int i = 1; i < dirVector.Length; i++)
-            {
-                if (dirPreference[i] > dirPreference[index])
-                    index = i;
-            }
-            nextDest = (Vector2)transform.position + dirVector[index] 
-                        * UnityEngine.Random.Range(minPatrolDist, maxPatrolDist);
-        }
-
         Vector2 moveDir = (dest - (Vector2)transform.position);
         if (moveDir.sqrMagnitude > 1)
             moveDir.Normalize();
@@ -121,7 +121,7 @@ public class AITask_AirPatrol : MonoBehaviour
             // 타이머 시작
             waitTimer = Timer.StartTimer();
         }
-        else if (waitTimer.duration >= patrolWaitTime)
+        else if (waitTimer.duration >= patrolStopTime)
         {
             // 타이머 시간 다 되었으면 PatrolWait 종료
             waitTimer = null;
@@ -134,8 +134,42 @@ public class AITask_AirPatrol : MonoBehaviour
     [Task]
     private void SetNextDest()
     {
+        UpdatePreference();
         UpdateNextDest();
+        MoveTerrainCheckCollider();
         ThisTask.Succeed();
+    }
+
+    private void UpdatePreference()
+    {
+        // 선호도 초기화
+        {
+            for (int i = 0; i < dirVector.Length; i++)
+            {
+                dirPreference[i] = 0.5f;        // 기본 선호도 0.5f
+            }
+        }
+        // 센터에서 멀어졌을 때 중심 쪽으로 가도록 선호도 증가
+        Vector2 toPatrolCenter = patrolAreaCenter - (Vector2)transform.position;         // 멀어졌을 때 선호도 크도록 normalize 안함.
+        for (int i = 0; i < dirVector.Length; i++)
+        {
+            dirPreference[i] += (Vector2.Dot(dirVector[i], toPatrolCenter) - 0.2f) * aiCoef_toCenter;
+            // 목표 방향과 수직인 요소 추가
+            dirPreference[i] += (1 - Mathf.Abs(Vector2.Dot(dirVector[i], toPatrolCenter))) * aiCoef_sidestep;
+        }
+        // 벽과 가까워졌을 때 벽과 가까운 방향일수록 선호도 감소
+        {
+            for (int i = 0; i < dirVector.Length; i++)
+            {
+                RaycastHit2D rayhit = Physics2D.Raycast(
+                            transform.position, dirVector[i], 5f,       // 벽과의 거리 최대 5까지 탐지
+                            LayerMask.GetMask("Ground"));
+                if (rayhit.collider != null)
+                {
+                    dirPreference[i] -= 1 / rayhit.distance * aiCoef_awayFromWall;
+                }
+            }
+        }
     }
 
     private void UpdateNextDest()
@@ -148,7 +182,7 @@ public class AITask_AirPatrol : MonoBehaviour
             for (int i = 0; i < dirVector.Length; i++)
             {
                 if (dirPreference[i] > 0)
-                    randomPicker.Add(i, Mathf.Pow(dirPreference[i], randomPow));
+                    randomPicker.Add(i, Mathf.Pow(dirPreference[i], randomWeightExp));    // 가중치 높은 게 더 잘 선택되도록 pow
             }
             index = randomPicker.GetRandomPick();
         }
@@ -165,63 +199,24 @@ public class AITask_AirPatrol : MonoBehaviour
         nextDest = (Vector2)transform.position + dirVector[index] * UnityEngine.Random.Range(minPatrolDist, maxPatrolDist);
     }
 
-
-    // 순찰할 다음 위치 지정은 원래 매 프레임 수행할 게 아니라 SetNextDest에서만 수행하지만
-    // 연구중에는 시각화를 위해 Update에서 수행하도록 하고 SetNextDest는 결과를 가져가기만 함.
-    private void Update()
+    private void MoveTerrainCheckCollider()
     {
-        // 선호도 초기화
-        {
-            for(int i = 0; i < dirVector.Length; i++)
-            {
-                dirPreference[i] = 0.5f;        // 기본 선호도 0.5f
-            }
-        }
-        // 순찰 범위가 제한되어 있다면 센터에서 멀어졌을 때 중심 쪽으로 가도록 선호도 증가
-        if (moveOnlyNearby)
-        {
-            // 멀어졌을 때 선호도 크도록 normalize 안함.
-            Vector2 toPatrolCenter = patrolAreaCenter - (Vector2)transform.position;
-            for(int i=0; i<dirVector.Length; i++)
-            {
-                dirPreference[i] += (Vector2.Dot(dirVector[i], toPatrolCenter) - 0.2f) * aiCoef_toCenter;
-                // 목표 방향과 수직인 요소 추가
-                dirPreference[i] += (1 - Mathf.Abs(Vector2.Dot(dirVector[i], toPatrolCenter))) * aiCoef_sidestep;
-            }
-        }
-        else
-        {
-            for(int i=0; i<dirVector.Length; i++)
-            {
-                dirPreference[i] += 1;
-            }
-        }
-        // 벽과 가까워졌을 때 벽과 가까운 방향일수록 선호도 감소
-        {
-            for(int i=0; i<dirVector.Length; i++)
-            {
-                RaycastHit2D rayhit = Physics2D.Raycast(
-                            transform.position, dirVector[i], 5f,       // 벽과의 거리 최대 5까지 탐지
-                            LayerMask.GetMask("Ground"));
-                if(rayhit.collider != null)
-                {
-                    dirPreference[i] -= 1 / rayhit.distance * aiCoef_awayFromWall;
-                }
-            }
-        }
+        Vector2 toNextDest = nextDest - (Vector2)transform.position;
+        terrainSensor.transform.localPosition = toNextDest.normalized * terrainSensorOffset;
+        blackboard.Set(BBK.StuckAtWall, false);     // 벽에 막혔음 플래그 강제 초기화
     }
 
-    private void OnDrawGizmos()
+    private void OnDrawGizmosSelected()
     {
-        if(moveOnlyNearby)
+        if (drawPatrolCenter)               // 순찰 중심점 표시
         {
             if (Application.isPlaying)
-                Gizmos.DrawWireSphere(patrolAreaCenter, 1.0f);
+                Gizmos.DrawWireSphere(patrolAreaCenter, 0.5f);
             else
-                Gizmos.DrawWireSphere(transform.position, 1.0f);
+                Gizmos.DrawWireSphere(transform.position, 0.5f);
         }
 
-        if(drawDestDecisionVector)
+        if(drawDestDecision)          // 다음 방향 선호도 표시
         {
             for(int i=0;i<dirVector.Length;i++)
             {
@@ -229,18 +224,23 @@ public class AITask_AirPatrol : MonoBehaviour
                 {
                     Gizmos.color = Color.green;
                     Gizmos.DrawLine((Vector2)transform.position + dirVector[i] * 0.5f,
-                        (Vector2)transform.position + dirVector[i] * (0.5f + dirPreference[i] * 0.2f));
+                        (Vector2)transform.position + dirVector[i] 
+                            * (0.5f + Mathf.Pow(dirPreference[i] * 0.2f, randomWeightExp)));
                 }
                 else
                 {
                     Gizmos.color = Color.red;
                     Gizmos.DrawLine((Vector2)transform.position + dirVector[i] * 0.5f,
-                        (Vector2)transform.position + dirVector[i] * (0.5f - dirPreference[i] * 0.2f));
+                        (Vector2)transform.position + dirVector[i]
+                            * (0.5f - Mathf.Pow(dirPreference[i] * 0.2f, randomWeightExp)));
                 }
             }
         }
 
-        Gizmos.color = Color.red;
-        Gizmos.DrawSphere(nextDest, 0.4f);
+        if(drawDestPoint)                    // 현재 순찰 목적지 표시
+        { 
+            Gizmos.color = Color.red;
+            Gizmos.DrawSphere(nextDest, 0.4f);
+        }
     }
 }
