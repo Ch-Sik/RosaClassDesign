604b76948d4ba7970f3cdde1e5cda36534417ad3
Blueshap <50543916+BlueShap@users.noreply.github.com>
2024-02-01 17:29:09 +0900

    [add] IgnoreCollision, BreakablePlatform
    충돌 무시 추가, 부서지는 플랫폼 추가

--- Assets/Scripts/Monster/Common/MonsterDamageInflictor.cs
+++ Assets/Scripts/Monster/Common/MonsterDamageInflictor.cs
@@ -6,6 +6,7 @@ using UnityEngine;
 public class MonsterDamageInflictor : MonoBehaviour
 {
     public int damage;
+    public float ignoreDuration = 2f; 
     private void OnCollisionEnter2D(Collision2D collision)
     {
         DoDamageIfItsPlayer(collision.gameObject);
@@ -26,6 +27,22 @@ public class MonsterDamageInflictor : MonoBehaviour
                     gameObject.transform.position.y - (gameObject.transform.localScale.y / 2),
                     gameObject.transform.position.z));
             go.GetComponent<PlayerState>().TakeDamage(damage);
+
+            int originalLayer = gameObject.layer;
+            int collisionLayer = go.layer;
+
+            // 현재 게임 오브젝트와 충돌한 오브젝트의 충돌을 무시
+            Physics2D.IgnoreLayerCollision(originalLayer, collisionLayer, true);
+
+            // 일정 시간 후 충돌 무시 해제
+            StartCoroutine(RestoreCollision(originalLayer, collisionLayer, ignoreDuration));
         }
+    }
+    IEnumerator RestoreCollision(int originalLayer, int collisionLayer, float delay)
+    {
+        yield return new WaitForSeconds(delay);
+
+        // 충돌 무시 해제
+        Physics2D.IgnoreLayerCollision(originalLayer, collisionLayer, false);
     }
 }
