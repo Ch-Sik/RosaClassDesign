199cd22bccd241ee418ae94ba72f1f705c202534
김태현 <th2252@gmail.com>
2024-07-27 21:20:13 +0900

    [Fix] 발사꽃 방향 지정 오류 수정
    고정방향 발사꽃이 회전된 상태에서 목표를 향해 투사체를 발사하지 못하던 문제를 수정
    게임이 시작되는 시점에 고정방향 발사꽃이 아직 '기상' 중인 애니메이션인데도 불구하고 투사체가 발사되던 현상 수정
    애니메이션에 '조준'이 반영되는 속도 조절 옵션 추가

--- Assets/Scripts/Monster/AITask/Task_NoSleep.cs
+++ Assets/Scripts/Monster/AITask/Task_NoSleep.cs
@@ -6,7 +6,11 @@ public class Task_NoSleep : MonoBehaviour
 {
     [InfoBox("Sleep을 사용한 BT를 재활용하기 위한 가짜 Sleep task. 실행되면 무조건 즉시 Fail한다.")]
     Blackboard blackboard;
-    // Start is called before the first frame update
+
+    [Tooltip("대기 모드에서 깨어나는 동작 시간 (초)")]
+    [SerializeField] private float wakeupDuration = 0.5f;
+
+    private Timer timer;
 
     private void Start()
     {
@@ -20,7 +24,17 @@ public class Task_NoSleep : MonoBehaviour
     [Task]
     protected void Sleep()
     {
-        blackboard.Set(BBK.isWokeUp, true);
-        ThisTask.Fail();
+        if (timer == null)
+        {
+            timer = Timer.StartTimer();
+            blackboard.Set(BBK.isWokeUp, true);
+        }
+
+        if (timer.duration > wakeupDuration)
+        {
+            timer = null;
+            // 실제 AI 행동을 막기 위해 Fail로 task 끝나는 거는 타이머 끝나고 수행
+            ThisTask.Fail();
+        }
     }
 }
