69b8c5366c0b3548e073889395c86d64853320cf
jadestar01 <104671580+jadestar01@users.noreply.github.com>
2024-08-25 01:48:09 +0900

    Fix 레버

--- Assets/Scripts/Gimmicks/SignalSenders/G_Lever.cs
+++ Assets/Scripts/Gimmicks/SignalSenders/G_Lever.cs
@@ -5,21 +5,72 @@ using UnityEngine;
 
 public class G_Lever : GimmickSignalSender
 {
+    public bool isOnce = true;
     [SerializeField] Transform leverHandle;
+
+    public bool onAct = false;
 
     private IEnumerator ActivateLever()
     {
         DOTween.Sequence()
-            .Append(leverHandle.DORotate(new Vector3(0, 0, -90), 0.4f, RotateMode.FastBeyond360).SetRelative(true))
-            .AppendCallback(() => SendSignal());
+            .AppendCallback(() => onAct = true)
+            .Append(leverHandle.DORotate(new Vector3(0, 0, -90), 0.4f, RotateMode.LocalAxisAdd).SetRelative(true))
+            .AppendCallback(() =>
+            {
+                onAct = false;
+                SendSignal();
+            });
         yield return 0;
+    }
+
+    private IEnumerator InActivateLever()
+    {
+        DOTween.Sequence()
+            .AppendCallback(() => onAct = true)
+            .Append(leverHandle.DORotate(new Vector3(0, 0, +90), 0.4f, RotateMode.LocalAxisAdd).SetRelative(true))
+            .AppendCallback(() =>
+            {
+                onAct = false;
+                SendSignal();
+            });
+        yield return 0;
+    }
+
+    public void LeverAction()
+    {
+        Debug.Log("11");
+
+        if (onAct)
+            return;
+
+        Debug.Log("22");
+
+        if (isActive)
+        {
+            Debug.Log("33");
+            if (isOnce)
+                return;
+            Debug.Log("44");
+            InActiveLever();
+        }
+        else
+        {
+            Debug.Log("55");
+            ActiveLever();
+        }
     }
 
     public void ActiveLever()
     {
-        if (isActive) return;        // 루틴 중복 실행 방지
-
         isActive = true;
         StartCoroutine(ActivateLever());
+        Debug.Log("66");
+    }
+
+    public void InActiveLever()
+    {
+        isActive = false;
+        StartCoroutine(InActivateLever());
+        Debug.Log("77");
     }
 }
