20e2d1fe533d4049a49e8ea674512a78952048e4
jadestar01 <104671580+jadestar01@users.noreply.github.com>
2024-08-25 00:15:06 +0900

    Fix 추락하는 큐브
    Fix 추락하는 큐브

--- Assets/Scripts/Gimmicks/Old/MovePlatformCollider.cs
+++ Assets/Scripts/Gimmicks/Old/MovePlatformCollider.cs
@@ -12,7 +12,14 @@ public class MovePlatformCollider : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if(IsOnTopOfPlatform(collision))
+        if (collision.transform.CompareTag("Player") && IsOnTopOfPlatform(collision))
+        {
+            if (platform != null)
+            {
+                platform.HandleChildTriggerEnter(collision.transform);
+            }
+        }
+        else if (collision.transform.CompareTag("Cube") && IsCubeOnTopOfPlatform(collision))
         {
             if (platform != null)
             {
@@ -20,6 +27,18 @@ public class MovePlatformCollider : MonoBehaviour
             }
         }
     }
+
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        if (collision.transform.CompareTag("Cube") && IsCubeOnTopOfPlatform(collision))
+        {
+            if (platform != null)
+            {
+                platform.HandleChildTriggerEnter(collision.transform);
+            }
+        }
+    }
+
     private void OnCollisionExit2D(Collision2D collision)
     {
         if (platform != null)
@@ -37,4 +56,12 @@ public class MovePlatformCollider : MonoBehaviour
         return objectBottomY > platformTopY;
     }
 
+    bool IsCubeOnTopOfPlatform(Collision2D collision)
+    {
+        float platformTopY = transform.position.y + GetComponent<Collider2D>().bounds.size.y / 2;
+
+        float cubeBottomY = collision.transform.position.y - collision.transform.GetComponent<G_Cube>().boxCol.bounds.size.y / 2;
+
+        return cubeBottomY > platformTopY;
+    }
 }
